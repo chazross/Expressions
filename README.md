276984bd2584b80951daf01ec5650847731cfeb7
Chaz Ross <chazross77@gmail.com>
2024-08-26 13:31:31 -0400

    Updated css

---
+++ README.md
@@ -0,0 +1,20 @@
+# Expressions
+
+### Expressions is where art, words, and humor come together in one location to meet your expressive needs with ease.
+
+## Features
+- Nav bar drop down for easy access to pages
+- Easy to use interface
+- GIF page with a search bar for keywords
+
+### Designed for easy access to photos, quotes and GIF's to share through social media, text, E-mail and more. 
+
+## Technologies:
+- HTML
+- SCSS
+- CSS
+- JS
+
+### Ideas for improvement:
+ - Pagentation bar
+ - Use of SCSS
---
+++ js/main.js
@@ -0,0 +1,51 @@
+const apiKey = 'lI4hgkjnIVmhO9JGrhuI9VLdWVDJxqeE'; 
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const gifContainer = document.getElementById('gif-container');
+
+
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value.trim();
+    if (query) {
+        fetchGifs(query);
+    }
+});
+
+async function fetchGifs(query) {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`);
+        const data = await response.json();
+        displayGifs(data.data);
+    } catch (error) {
+        console.error('Error fetching GIFs:', error);
+    }
+}
+
+function displayGifs(gifs) {
+    gifContainer.innerHTML = '';
+    gifs.forEach(gif => {
+        const gifItem = document.createElement('div');
+        gifItem.className = 'gif-item';
+        gifItem.innerHTML = `<img src="${gif.images.fixed_height.url}" alt="${gif.title}">`;
+        gifContainer.appendChild(gifItem);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navbarMenu = document.getElementById('navbar-menu');
+  
+    hamburgerMenu.addEventListener('click', () => {
+      navbarMenu.classList.toggle('active');
+    });
+  });
+
+
+
+
+
+
+
+
+
+ 
